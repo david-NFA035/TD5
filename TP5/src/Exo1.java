7addbad359562d0b25c2298b5294a63815e0ce91
david <david.bertrand@sudest.pleiad.net>
2014-04-16 00:21:48 +0200

    exo1

---
+++ TP5/src/Exo1.java
@@ -0,0 +1,54 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+public class Exo1 {
+
+	public static void main(String[] args) {
+		String[] tab = {"un",
+				        "deux",
+				        "trois",
+				        "quatre",
+				        "cinq"
+		};
+		
+		ArrayList<String> ac = new ArrayList<String>();
+		for (String s : tab){
+			ac.add(s);
+		}
+		HashSet<String> sc = new HashSet<String>();
+		for (String s : tab){
+			sc.add(s);
+		}
+		afficheCollV1(ac);
+		afficheCollV1(sc);
+		afficheCollV2(ac);
+		afficheCollV2(sc);
+
+
+	}
+	
+	public static void afficheCollV1(Collection<String> c){
+		
+		System.out.println("Affichage d'un" + (c instanceof ArrayList<?> ? "e Arraylist " : " HashSet") + "avec une boucle for each :\n");
+		for (String s : c){
+			System.out.println(s);
+		}
+		System.out.println();
+	}
+	
+    public static void afficheCollV2(Collection<String> c){
+    	System.out.println("Affichage d'un" + (c instanceof ArrayList<?> ? "e Arraylist " : " HashSet") + "avec un itérateur :\n");
+    	Iterator<String> i = c.iterator();
+    	while (i.hasNext()){
+    		System.out.println(i.next());
+    	}
+    	System.out.println();
+		
+	}
+
+}
+
+
